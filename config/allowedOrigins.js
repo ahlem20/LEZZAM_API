a4bafe54b40ad5741cc03ee71172601d659e7ec0
ahlem20 <107472826+ahlem20@users.noreply.github.com>
2024-09-09 10:20:47 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://lezzamfrontend1.onrender.com/'
+    'https://lezzamfrontend1.onrender.com/',
+    'https://lezzamapp.onrender.com/'
 ]
 
 module.exports = allowedOrigins
